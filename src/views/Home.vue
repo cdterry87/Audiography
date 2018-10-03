ea09ae61f0baceb38111c56415835d8b7861b31f
Chase Terry <chase.terry87@gmail.com>
2018-10-02 23:39:33 -0500

    First commit after setting up Vue with Vuetify and initializing the search functionality.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,97 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container fluid grid-list-md text-xs-center>
+      <v-layout row wrap>
+          <v-flex md6 offset-md3>
+              <v-card>
+                <h1>Search</h1>
+                <v-card-text>
+                    <v-form v-model="valid" @submit.prevent="search">
+                        <v-text-field v-model="searchField" :rules="searchRules" :counter="3" label="Search" required></v-text-field>
+                        <v-btn :disabled="!valid" @click="search">submit</v-btn>
+                        <v-btn @click="clear">clear</v-btn>
+                    </v-form>
+                </v-card-text>
+              </v-card>
+          </v-flex>
+      </v-layout>
+      <br>
+      <v-layout row v-if="searchResults.length > 0">
+          <v-flex md8 offset-md2>
+              <v-card>
+                  <h2>Search Results</h2>
+                  <v-list two-line>
+                      <v-list-tile avatar v-for="result in searchResults" :key="result.idArtist">
+                          <v-list-tile-avatar>
+                                <v-icon>{{ result.strArtistThumb }}</v-icon>
+                          </v-list-tile-avatar>
+                          <v-list-tile-content>
+                                <v-list-tile-title>{{ result.strArtist }}</v-list-tile-title>
+                                <v-list-tile-sub-title>{{ result.strBiographyEN }}</v-list-tile-sub-title>
+                          </v-list-tile-content>
+                      </v-list-tile>
+                  </v-list>
+              </v-card>
+          </v-flex>
+      </v-layout>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    import axios from "axios";
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    export default {
+    data: () => ({
+        valid: false,
+        searchField: '',
+        searchResults: [],
+        searchRules: [
+            v => !!v || 'Search is required',
+            v => v.length >= 3 || 'Search field must contain at least 3 characters.'
+        ]
+    }),
+    methods: {
+        search() {
+            let that = this;
+            axios.get('https://theaudiodb.com/api/v1/json/1/search.php', {
+                params: {
+                    s: this.searchField
+                }
+            })
+            .then(function (response) {
+                console.log('received response!');
+                that.searchResults = response;
+
+                console.log('results', that.searchResults);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        },
+        clear() {
+            this.searchField = '';
+        }
+    },
+    created() {
+        
+    }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+    font-weight: normal;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+}
+</style>
