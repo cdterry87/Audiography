b71bd9f283bddf1bca25640ec4b57b46f4233a76
= <chase.terry87@gmail.com>
2018-11-08 15:03:04 -0600

    Revert "Fixed horizontal scrolling issue on mobile."
    This reverts commit 04b7555dc3141bfe076b3b58479a01dcf31af1b4.

--- src/App.vue
+++ src/App.vue
@@ -23,17 +23,6 @@ export default {
 </script>
 
 <style>
-html,
-body {
-    height: 100%;
-    width: 100%;
-    background: #f2f6fa !important;
-    overflow-x: hidden;
-}
-
-body {
-  position: relative
-}
 a {
     color: inherit !important;
     text-decoration: none;
